a53f9c7cdddfc4426000fb0a3b5e62fe18eb55df
Pdom <pdom0327@gmail.com>
2024-07-10 06:12:42 +0900

    feat: 3 body problem

--- astro-items/collectibles/customs/three-body-problem.lua
+++ astro-items/collectibles/customs/three-body-problem.lua
@@ -1,3 +1,11 @@
+---
+local SOUND_VOLUME = 1
+
+local BOSS_EXTRA_DAMAGE = 0.3
+---
+
+local isc = require("astro-items.lib.isaacscript-common")
+
 AstroItems.Collectible.THREE_BODY_PROBLEM = Isaac.GetItemIdByName("3 Body Problem")
 
 if EID then
@@ -7,7 +15,8 @@ end
 ---@type CollectibleType[]
 local activeItmes = {}
 
-local soundVoulme = 1
+---@type CollectibleType[]
+local banItems = {}
 
 AstroItems:AddCallback(
     ModCallbacks.MC_POST_GAME_STARTED,
@@ -15,7 +24,11 @@ AstroItems:AddCallback(
         -- AstroItems.Collectible 초기화 순서 문제로 인해 여기서 초기화합니다.
         activeItmes = {
             CollectibleType.COLLECTIBLE_BOOK_OF_BELIAL,
-            CollectibleType.COLLECTIBLE_BOOK_OF_SHADOWS
+            CollectibleType.COLLECTIBLE_BOOK_OF_SHADOWS,
+        }
+
+        banItems = {
+            CollectibleType.COLLECTIBLE_SPOON_BENDER,
         }
     end
 )
@@ -28,7 +41,7 @@ local function RunEffect(player)
 
     player:AnimateCollectible(AstroItems.Collectible.THREE_BODY_PROBLEM, "HideItem")
     player:UseActiveItem(item, false)
-    SFXManager():Play(SoundEffect.SOUND_DIVINE_INTERVENTION, soundVoulme)
+    SFXManager():Play(SoundEffect.SOUND_DIVINE_INTERVENTION, SOUND_VOLUME)
 end
 
 local isFirstKill = true
@@ -81,4 +94,40 @@ AstroItems:AddCallback(
             end
         end
     end
+)
+
+AstroItems:AddCallback(
+    ModCallbacks.MC_ENTITY_TAKE_DMG,
+    ---@param entity Entity
+    ---@param amount number
+    ---@param damageFlags number
+    ---@param source EntityRef
+    ---@param countdownFrames number
+    function(_, entity, amount, damageFlags, source, countdownFrames)
+        local player = AstroItems:GetPlayerFromEntity(source.Entity)
+
+        if player ~= nil and player:HasCollectible(AstroItems.Collectible.THREE_BODY_PROBLEM) then
+            if entity:IsBoss() and (source.Type == EntityType.ENTITY_TEAR or damageFlags & DamageFlag.DAMAGE_LASER == DamageFlag.DAMAGE_LASER or source.Type == EntityType.ENTITY_KNIFE) then
+                entity:TakeDamage(amount * BOSS_EXTRA_DAMAGE ^ player:GetCollectibleNum(AstroItems.Collectible.THREE_BODY_PROBLEM), 0, EntityRef(player), 0)
+            end
+        end
+    end
+)
+
+AstroItems:AddCallbackCustom(
+    isc.ModCallbackCustom.POST_PLAYER_COLLECTIBLE_ADDED,
+    ---@param player EntityPlayer
+    ---@param collectibleType CollectibleType
+    function(_, player, collectibleType)
+        if AstroItems:IsFirstAdded(AstroItems.Collectible.THREE_BODY_PROBLEM) then
+            for _, item in ipairs(banItems) do
+                AstroItems:RemoveAllCollectible(player, item)
+            end
+
+            player:AddCollectible(CollectibleType.COLLECTIBLE_LUDOVICO_TECHNIQUE)
+            player:AddCollectible(CollectibleType.COLLECTIBLE_LUDOVICO_TECHNIQUE)
+            player:AddCollectible(CollectibleType.COLLECTIBLE_LUDOVICO_TECHNIQUE)
+        end
+    end,
+    AstroItems.Collectible.THREE_BODY_PROBLEM
 )
