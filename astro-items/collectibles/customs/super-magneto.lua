25d9ad35653ad4b106bb70bfe75bdbab5c8c7ee8
Pdom <pdom0327@gmail.com>
2024-08-09 20:26:26 +0900

    feat: quick pick 대응

--- astro-items/collectibles/customs/super-magneto.lua
+++ astro-items/collectibles/customs/super-magneto.lua
@@ -1,5 +1,33 @@
+local isc = require("astro-items.lib.isaacscript-common")
+
 AstroItems.Collectible.SUPER_MAGNETO = Isaac.GetItemIdByName("Super Magneto")
 
 if EID then
     AstroItems:AddEIDCollectible(AstroItems.Collectible.SUPER_MAGNETO, "슈퍼 마그넷", "...", "Quickpick 모드의 사거리가 무한으로 변경됩니다.")
 end
+
+AstroItems:AddCallbackCustom(
+    isc.ModCallbackCustom.POST_PLAYER_COLLECTIBLE_ADDED,
+    ---@param player EntityPlayer
+    ---@param collectibleType CollectibleType
+    function(_, player, collectibleType)
+        if not QP_OVERWRITE then
+            QP_OVERWRITE = {}
+        end
+
+        QP_OVERWRITE.isInfinity = true
+    end,
+    AstroItems.Collectible.SUPER_MAGNETO
+)
+
+AstroItems:AddCallbackCustom(
+    isc.ModCallbackCustom.POST_PLAYER_COLLECTIBLE_REMOVED,
+    ---@param player EntityPlayer
+    ---@param collectibleType CollectibleType
+    function(_, player, collectibleType)
+        if QP_OVERWRITE then
+            QP_OVERWRITE.isInfinity = false
+        end
+    end,
+    AstroItems.Collectible.SUPER_MAGNETO
+)
