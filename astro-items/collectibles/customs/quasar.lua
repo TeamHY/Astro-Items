f9fba7f606499600ce4bcc44055faf255c6cc495
Pdom <pdom0327@gmail.com>
2024-10-11 21:39:52 +0900

    fix(quasar): 의도대로 수정

--- astro-items/collectibles/customs/quasar.lua
+++ astro-items/collectibles/customs/quasar.lua
@@ -1,8 +1,14 @@
 AstroItems.Collectible.QUASAR = Isaac.GetItemIdByName("Quasar")
 
 if EID then
-    AstroItems:AddEIDCollectible(AstroItems.Collectible.QUASAR, "퀘이사", "...",
-        "방 마다 처음으로 죽은 적 위치에 블랙홀을 소환합니다. 중첩 시 여러 번 발동할 수 있습니다.#클리어 되지 않은 방 입장 시 10초 동안 무적이 됩니다. 중첩 시 지속시간이 증가합니다.#방 클리어 시 10초 동안 무적이 됩니다.")
+    AstroItems:AddEIDCollectible(
+        AstroItems.Collectible.QUASAR,
+        "퀘이사",
+        "...",
+        "방 마다 처음으로 죽은 적 위치에 블랙홀을 소환합니다." ..
+        "#방 클리어 시 10초 동안 무적이 됩니다." ..
+        "#2개 이상 소지 방 입장 시에도 10초 동안 무적이 됩니다."
+    )
 end
 
 local remaining = 0
@@ -10,18 +16,14 @@ local remaining = 0
 AstroItems:AddCallback(
     ModCallbacks.MC_POST_NEW_ROOM,
     function(_)
-        remaining = 0
+        remaining = 1
 
         for i = 1, Game():GetNumPlayers() do
             local player = Isaac.GetPlayer(i - 1)
             local quasarNum = player:GetCollectibleNum(AstroItems.Collectible.QUASAR)
 
-            remaining = remaining + player:GetCollectibleNum(AstroItems.Collectible.QUASAR)
-
-            if not Game():GetRoom():IsClear() then
-                for _ = 1, quasarNum do
-                    player:UseActiveItem(CollectibleType.COLLECTIBLE_BOOK_OF_SHADOWS, false)
-                end
+            if not Game():GetRoom():IsClear() and quasarNum >= 2 then
+                player:UseActiveItem(CollectibleType.COLLECTIBLE_BOOK_OF_SHADOWS, false)
             end
         end
     end
