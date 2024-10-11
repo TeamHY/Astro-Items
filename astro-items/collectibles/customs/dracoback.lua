f2002d8bc4569d85c28dfb0381d5db4dd85fbcc9
Pdom <pdom0327@gmail.com>
2024-10-12 03:22:32 +0900

    feat(dracoback): 용사 캐릭터 시너지, 보스방 예외

--- astro-items/collectibles/customs/dracoback.lua
+++ astro-items/collectibles/customs/dracoback.lua
@@ -5,7 +5,8 @@ if EID then
         AstroItems.Collectible.DRACOBACK,
         "기룡 드라코백",
         "...",
-        "방 입장 시 적 하나가 지워집니다. 중첩 시 지워지는 적의 수가 증가합니다."
+        "방 입장 시 적 하나가 지워집니다. {{BossRoom}}보스방에서는 발동하지 않습니다. 중첩 시 지워지는 적의 수가 증가합니다." ..
+        "#성전의 수견사, 일리걸 나이트일 경우 지워지는 적의 수가 하나 증가합니다."
     )
 end
 
@@ -14,15 +15,21 @@ AstroItems:AddCallback(
     function(_)
         for i = 1, Game():GetNumPlayers() do
             local player = Isaac.GetPlayer(i - 1)
+            local room = Game():GetRoom();
 
-            if not Game():GetRoom():IsClear() then
+            if not room:IsClear() and room:GetType() ~= RoomType.ROOM_BOSS then
                 local entities = AstroItems:Filter(Isaac.GetRoomEntities(), function(entity)
                     return entity:IsVulnerableEnemy() and not entity:IsBoss() and entity.Type ~= EntityType.ENTITY_FIREPLACE
                 end)
 
                 local rng = player:GetCollectibleRNG(AstroItems.Collectible.DRACOBACK)
+                local dracobackNum = player:GetCollectibleNum(AstroItems.Collectible.DRACOBACK)
 
-                for _ = 1, math.min(#entities, player:GetCollectibleNum(AstroItems.Collectible.DRACOBACK)) do
+                if AstroItems:IsWaterEnchantress(player) then
+                    dracobackNum = dracobackNum + 1
+                end
+
+                for _ = 1, math.min(#entities, dracobackNum) do
                     local index = rng:RandomInt(#entities) + 1
                     local entity = entities[index]
 
