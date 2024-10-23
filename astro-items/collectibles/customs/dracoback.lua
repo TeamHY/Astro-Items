7425ded6c8f3dd8d52ec15292aaedd6abb7cfbaa
Pdom <pdom0327@gmail.com>
2024-10-23 20:54:09 +0900

    fix(dracoback): 수견사 시너지 변경

--- astro-items/collectibles/customs/dracoback.lua
+++ astro-items/collectibles/customs/dracoback.lua
@@ -1,3 +1,9 @@
+---
+
+local waterEnchantressMultiplier = 1.1
+
+---
+
 AstroItems.Collectible.DRACOBACK = Isaac.GetItemIdByName("Dracoback, the Rideable Dragon")
 
 if EID then
@@ -6,7 +12,7 @@ if EID then
         "기룡 드라코백",
         "...",
         "방 입장 시 적 하나가 지워집니다. {{BossRoom}}보스방에서는 발동하지 않습니다. 중첩 시 지워지는 적의 수가 증가합니다." ..
-        "#성전의 수견사, 일리걸 나이트일 경우 지워지는 적의 수가 하나 증가합니다."
+        "#성전의 수견사, 일리걸 나이트일 경우 올스탯이 x1.1 증가합니다."
     )
 end
 
@@ -15,6 +21,11 @@ AstroItems:AddCallback(
     function(_)
         for i = 1, Game():GetNumPlayers() do
             local player = Isaac.GetPlayer(i - 1)
+
+            if not player:HasCollectible(AstroItems.Collectible.DRACOBACK) then
+                goto continue
+            end
+            
             local room = Game():GetRoom();
 
             if not room:IsClear() and room:GetType() ~= RoomType.ROOM_BOSS then
@@ -24,10 +35,6 @@ AstroItems:AddCallback(
 
                 local rng = player:GetCollectibleRNG(AstroItems.Collectible.DRACOBACK)
                 local dracobackNum = player:GetCollectibleNum(AstroItems.Collectible.DRACOBACK)
-
-                if AstroItems:IsWaterEnchantress(player) then
-                    dracobackNum = dracobackNum + 1
-                end
 
                 for _ = 1, math.min(#entities, dracobackNum) do
                     local index = rng:RandomInt(#entities) + 1
@@ -45,6 +52,44 @@ AstroItems:AddCallback(
                     table.remove(entities, index)
                 end
             end
+
+            ::continue::
+        end
+    end
+)
+
+AstroItems:AddPriorityCallback(
+    ModCallbacks.MC_EVALUATE_CACHE,
+    CallbackPriority.LATE + 2000, -- 혼줌과 동일
+    ---@param player EntityPlayer
+    ---@param cacheFlag CacheFlag
+    function (_, player, cacheFlag)
+        if not player:HasCollectible(AstroItems.Collectible.DRACOBACK) then return end
+
+        if not AstroItems:IsWaterEnchantress(player) then return end
+
+        if cacheFlag == CacheFlag.CACHE_FIREDELAY then
+            player.MaxFireDelay = ((player.MaxFireDelay + 1) / waterEnchantressMultiplier) - 1
+        end
+
+        if cacheFlag == CacheFlag.CACHE_DAMAGE then
+            player.Damage = player.Damage * waterEnchantressMultiplier
+        end
+
+        if cacheFlag == CacheFlag.CACHE_SPEED then
+            player.MoveSpeed = player.MoveSpeed * waterEnchantressMultiplier
+        end
+
+        if cacheFlag == CacheFlag.CACHE_LUCK and player.Luck > 0 then
+            player.Luck = player.Luck * waterEnchantressMultiplier
+        end
+
+        if cacheFlag == CacheFlag.CACHE_RANGE then
+            player.TearRange = player.TearRange * waterEnchantressMultiplier
+        end
+
+        if cacheFlag == CacheFlag.CACHE_SHOTSPEED then
+            player.ShotSpeed = player.ShotSpeed * waterEnchantressMultiplier
         end
     end
 )
