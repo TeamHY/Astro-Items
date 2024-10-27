0939cec7f1ac45af1d379b5c310cd63a800a08f1
Pdom <pdom0327@gmail.com>
2024-10-28 04:41:46 +0900

    wip(rgb): 미완성2

--- astro-items/collectibles/customs/rgb.lua
+++ astro-items/collectibles/customs/rgb.lua
@@ -20,7 +20,7 @@ AstroItems:AddCallback(
             local player = Isaac.GetPlayer()
             local rng = player:GetCollectibleRNG(AstroItems.Collectible.RGB)
 
-            local noise = AstroItems.Noise:GetWhiteNoise(rng:GetSeed(), room:GetSpawnSeed(), 1)
+            local noise = AstroItems.Noise:GetWhiteNoise(rng:GetSeed(), level:GetCurrentRoomIndex(), 1)
             
             if noise < 1 / 3 then
                 room:SetWallColor(Color(1, 1, 1, 1, 255, 0, 0))
