6196d6dc4570e466d54aeffa3531f1529f811c28
Pdom <pdom0327@gmail.com>
2024-12-26 05:36:55 +0900

    feat(ez-mode): 행운 감소 #482

--- astro/collectibles/customs/ez-mode.lua
+++ astro/collectibles/customs/ez-mode.lua
@@ -1,7 +1,7 @@
 Astro.Collectible.EZ_MODE = Isaac.GetItemIdByName("EZ Mode")
 
 if EID then
-    Astro:AddEIDCollectible(Astro.Collectible.EZ_MODE, "쉬운 모드", "...", "후반 스테이지 진입 전까지 피격 페널티가 발생하지 않습니다.#소울 하트 1개가 증가됩니다.")
+    Astro:AddEIDCollectible(Astro.Collectible.EZ_MODE, "쉬운 모드", "...", "↓ {{LuckSmall}}행운 -2#후반 스테이지 진입 전까지 피격 페널티가 발생하지 않습니다.#소울 하트 1개가 증가됩니다.")
 end
 
 Astro:AddCallback(
@@ -23,4 +23,16 @@ Astro:AddCallback(
             end
         end
     end
+)
+
+Astro:AddPriorityCallback(
+    ModCallbacks.MC_EVALUATE_CACHE,
+    ---@param player EntityPlayer
+    ---@param cacheFlag CacheFlag
+    function (_, player, cacheFlag)
+        if player:HasCollectible(Astro.Collectible.EZ_MODE) then
+            player.Luck = player.Luck - 2
+        end
+    end,
+    CacheFlag.CACHE_LUCK
 )
