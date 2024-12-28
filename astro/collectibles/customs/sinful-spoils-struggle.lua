d93237269f92f14e52938cb5e6e6ceedfcba8d5b
Pdom <pdom0327@gmail.com>
2024-12-29 03:02:07 +0900

    feat(sinful-spoils-struggle): 효과 변경

--- astro/collectibles/customs/sinful-spoils-struggle.lua
+++ astro/collectibles/customs/sinful-spoils-struggle.lua
@@ -1,125 +1,80 @@
+local isc = require("astro.lib.isaacscript-common")
+local hiddenItemManager = require("astro.lib.hidden_item_manager")
+
 Astro.Collectible.SINFUL_SPOILS_STRUGGLE = Isaac.GetItemIdByName("Sinful Spoils Struggle")
 
-local useSound = Isaac.GetSoundIdByName('Destroyed')
-local useSoundVoulme = 1 -- 0 ~ 1
-
 Astro:AddCallback(
-    ModCallbacks.MC_POST_GAME_STARTED,
-    function(_, isContinued)
+    Astro.Callbacks.MOD_INIT,
+    function(_)
         if EID then
             Astro:AddEIDCollectible(
                 Astro.Collectible.SINFUL_SPOILS_STRUGGLE,
                 "죄보합전",
                 "...",
-                "일급 비밀방에서 사용 시 소지된 아이템 중 1개를 제거합니다. {{Collectible" .. Astro.Collectible.ORIGINAL_SINFUL_SPOILS_SNAKE_EYE .. "}}Original Sinful Spoils - Snake Eye, {{Collectible" .. Astro.Collectible.SINFUL_SPOILS_OF_SUBVERSION_SNAKE_EYE .. "}}Sinful Spoils of Subversion - Snake Eye 중 하나와 제거된 아이템을 소환합니다. 하나를 선택하면 나머지는 사라집니다.#스테이지 진입 시 쿨타임이 채워집니다.#디아벨스타, 디아벨제가 아닐 경우 사용 시 {{LuckSmall}}행운이 1 감소하고 제거 후 소환되는 아이템이 2개로 증가합니다."
+                "{{Collectible684}}Hungry Soul 효과가 적용됩니다." ..
+                "{{Collectible634}}Purgatory 효과가 적용됩니다." ..
+                "중첩 시 {{Collectible727}}Ghost Bombs 효과가 적용됩니다." ..
+                "소환된 유령의 속도가 2배 빨라집니다."
             )
-        end
-
-        if not isContinued then
-            Astro.Data.SinfulSpoilsStruggle = {
-                luck = 0
-            }
         end
     end
 )
 
 Astro:AddCallback(
-    ModCallbacks.MC_POST_NEW_LEVEL,
+    ModCallbacks.MC_POST_UPDATE,
     function(_)
-        for i = 1, Game():GetNumPlayers() do
-            local player = Isaac.GetPlayer(i - 1)
+        if Astro:HasCollectible(Astro.Collectible.SINFUL_SPOILS_STRUGGLE) then
+            local effects = Isaac.FindByType(EntityType.ENTITY_EFFECT)
 
-            for j = 0, ActiveSlot.SLOT_POCKET2 do
-                if player:GetActiveItem(j) == Astro.Collectible.SINFUL_SPOILS_STRUGGLE then
-                    if player:GetPlayerType() == Astro.Players.WATER_ENCHANTRESS and player:HasCollectible(CollectibleType.COLLECTIBLE_BIRTHRIGHT) then
-                        player:AddCollectible(CollectibleType.COLLECTIBLE_BATTERY)
-                        player:SetActiveCharge(100, j)
-                        player:RemoveCollectible(CollectibleType.COLLECTIBLE_BATTERY)
-                    else
-                        player:SetActiveCharge(50, j)
-                    end
+            for _, entity in ipairs(effects) do
+                if entity.Variant == EffectVariant.PURGATORY or entity.Variant == EffectVariant.HUNGRY_SOUL then
+                    entity:Update()
                 end
             end
         end
     end
 )
 
-
-Astro:AddCallback(
-    ModCallbacks.MC_USE_ITEM,
-    ---@param collectibleID CollectibleType
-    ---@param rngObj RNG
-    ---@param playerWhoUsedItem EntityPlayer
-    ---@param useFlags UseFlag
-    ---@param activeSlot ActiveSlot
-    ---@param varData integer
-    function(_, collectibleID, rngObj, playerWhoUsedItem, useFlags, activeSlot, varData)
-        if Game():GetLevel():GetCurrentRoom():GetType() ~= RoomType.ROOM_SUPERSECRET then
-            return {
-                Discharge = false,
-                Remove = false,
-                ShowAnim = false,
-            }
+Astro:AddCallbackCustom(
+    isc.ModCallbackCustom.POST_PLAYER_COLLECTIBLE_ADDED,
+    ---@param player EntityPlayer
+    ---@param collectibleType CollectibleType
+    function(_, player, collectibleType)
+        if not hiddenItemManager:Has(player, CollectibleType.COLLECTIBLE_HUNGRY_SOUL) then
+            hiddenItemManager:Add(player, CollectibleType.COLLECTIBLE_HUNGRY_SOUL)
         end
 
-        local inventory = Astro:getPlayerInventory(playerWhoUsedItem, false)
-        local rng = playerWhoUsedItem:GetCollectibleRNG(Astro.Collectible.SINFUL_SPOILS_STRUGGLE)
-
-        local hadCollectables = {}
-
-        if Astro:IsDiabellstar(playerWhoUsedItem) then
-            hadCollectables = Astro:GetRandomCollectibles(inventory, rng, 1, Astro.Collectible.SINFUL_SPOILS_STRUGGLE, true)
-        else
-            hadCollectables = Astro:GetRandomCollectibles(inventory, rng, 2, Astro.Collectible.SINFUL_SPOILS_STRUGGLE, true)
+        if not hiddenItemManager:Has(player, CollectibleType.COLLECTIBLE_PURGATORY) then
+            hiddenItemManager:Add(player, CollectibleType.COLLECTIBLE_PURGATORY)
         end
 
-        if hadCollectables[1] == nil then
-            return {
-                Discharge = false,
-                Remove = false,
-                ShowAnim = false,
-            }
+        if player:GetCollectibleNum(Astro.Collectible.SINFUL_SPOILS_STRUGGLE) > 1 then
+            if not hiddenItemManager:Has(player, CollectibleType.COLLECTIBLE_GHOST_BOMBS) then
+                hiddenItemManager:Add(player, CollectibleType.COLLECTIBLE_GHOST_BOMBS)
+            end
         end
-
-        for _, hadCollectable in ipairs(hadCollectables) do
-            Astro:SpawnCollectible(hadCollectable, playerWhoUsedItem.Position, Astro.Collectible.SINFUL_SPOILS_STRUGGLE)
-            playerWhoUsedItem:RemoveCollectible(hadCollectable)
-        end
-
-        local random = rng:RandomInt(2)
-
-        if random == 0 then
-            Astro:SpawnCollectible(Astro.Collectible.ORIGINAL_SINFUL_SPOILS_SNAKE_EYE, playerWhoUsedItem.Position, Astro.Collectible.SINFUL_SPOILS_STRUGGLE)
-        else
-            Astro:SpawnCollectible(Astro.Collectible.SINFUL_SPOILS_OF_SUBVERSION_SNAKE_EYE, playerWhoUsedItem.Position, Astro.Collectible.SINFUL_SPOILS_STRUGGLE)
-        end
-
-        SFXManager():Play(useSound, useSoundVoulme)
-
-        if not Astro:IsDiabellstar(playerWhoUsedItem) then
-            Astro.Data.SinfulSpoilsStruggle.luck = Astro.Data.SinfulSpoilsStruggle.luck - 1
-
-            playerWhoUsedItem:AddCacheFlags(CacheFlag.CACHE_LUCK)
-            playerWhoUsedItem:EvaluateItems()
-        end
-
-        return {
-            Discharge = true,
-            Remove = false,
-            ShowAnim = true,
-        }
     end,
     Astro.Collectible.SINFUL_SPOILS_STRUGGLE
 )
 
-Astro:AddCallback(
-    ModCallbacks.MC_EVALUATE_CACHE,
+Astro:AddCallbackCustom(
+    isc.ModCallbackCustom.POST_PLAYER_COLLECTIBLE_REMOVED,
     ---@param player EntityPlayer
-    ---@param cacheFlag CacheFlag
-    function(_, player, cacheFlag)
-        if not Astro:IsDiabellstar(player) and Astro.Data.SinfulSpoilsStruggle ~= nil then
-            player.Luck = player.Luck + Astro.Data.SinfulSpoilsStruggle.luck
+    ---@param collectibleType CollectibleType
+    function(_, player, collectibleType)
+        if hiddenItemManager:Has(player, CollectibleType.COLLECTIBLE_HUNGRY_SOUL) then
+            hiddenItemManager:Remove(player, CollectibleType.COLLECTIBLE_HUNGRY_SOUL)
+        end
+
+        if hiddenItemManager:Has(player, CollectibleType.COLLECTIBLE_PURGATORY) then
+            hiddenItemManager:Remove(player, CollectibleType.COLLECTIBLE_PURGATORY)
+        end
+
+        if player:GetCollectibleNum(Astro.Collectible.SINFUL_SPOILS_STRUGGLE) <= 1 then
+            if hiddenItemManager:Has(player, CollectibleType.COLLECTIBLE_GHOST_BOMBS) then
+                hiddenItemManager:Remove(player, CollectibleType.COLLECTIBLE_GHOST_BOMBS)
+            end
         end
     end,
-    CacheFlag.CACHE_LUCK
+    Astro.Collectible.SINFUL_SPOILS_STRUGGLE
 )
