7695c57bcfd54adac7412004a1c728a651bd9093
Pdom <pdom0327@gmail.com>
2025-03-10 14:37:48 +0900

    feat(birthright-eve): 신규 아이템

---
+++ astro/collectibles/customs/birthright-eve.lua
@@ -0,0 +1,54 @@
+local isc = require("astro.lib.isaacscript-common")
+
+Astro.Collectible.BIRTHRIGHT_EVE = Isaac.GetItemIdByName("Birthright - Eve")
+
+Astro:AddCallback(
+    Astro.Callbacks.MOD_INIT,
+    function(_)
+        if EID then
+            Astro:AddEIDCollectible(
+                Astro.Collectible.BIRTHRIGHT_EVE,
+                "생득권 - 이브",
+                "...",
+                "{{Collectible122}}Whore of babylon, {{Collectible122}}Dead Bird 효과가 항상 적용됩니다. 중첩이 가능합니다."
+            )
+        end
+    end
+)
+
+Astro:AddCallbackCustom(
+    isc.ModCallbackCustom.POST_PLAYER_COLLECTIBLE_ADDED,
+    ---@param player EntityPlayer
+    ---@param collectibleType CollectibleType
+    function(_, player, collectibleType)
+        local targetNum = player:GetCollectibleNum(Astro.Collectible.BIRTHRIGHT_EVE)
+        local effectNum = player:GetEffects():GetCollectibleEffectNum(CollectibleType.COLLECTIBLE_WHORE_OF_BABYLON)
+
+        if targetNum > effectNum then
+            local additionalEffects = targetNum - effectNum
+
+            player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_WHORE_OF_BABYLON, true, additionalEffects)
+            player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_DEAD_BIRD, true, additionalEffects)
+        end
+    end,
+    Astro.Collectible.BIRTHRIGHT_EVE
+)
+
+Astro:AddCallback(
+    ModCallbacks.MC_POST_NEW_ROOM,
+    function(_)
+        for i = 1, Game():GetNumPlayers() do
+            local player = Isaac.GetPlayer(i - 1)
+
+            local targetNum = player:GetCollectibleNum(Astro.Collectible.BIRTHRIGHT_EVE)
+            local effectNum = player:GetEffects():GetCollectibleEffectNum(CollectibleType.COLLECTIBLE_WHORE_OF_BABYLON)
+
+            if targetNum > effectNum then
+                local additionalEffects = targetNum - effectNum
+
+                player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_WHORE_OF_BABYLON, true, additionalEffects)
+                player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_DEAD_BIRD, true, additionalEffects)
+            end
+        end
+    end
+)
