21945453780089e01285d217dcd26459751f69b6
Pdom <pdom0327@gmail.com>
2025-05-07 22:03:14 +0900

    feat(cygnus): 피격 패널티 추가

--- astro/collectibles/customs/planetarium/cygnus.lua
+++ astro/collectibles/customs/planetarium/cygnus.lua
@@ -1,8 +1,21 @@
+---
+
+local PENALTY_TIME = 2 * 60 * 30
+
+---
+
 Astro.Collectible.CYGNUS = Isaac.GetItemIdByName("Cygnus")
 
 if EID then
     -- 15초 -> 7.5초 -> 5초
-    Astro:AddEIDCollectible(Astro.Collectible.CYGNUS, "백조자리", "...", "게임 시간 15초마다 2번 빛줄기를 소환합니다.#중첩 시 발동 간격이 줄어듭니다.")
+    Astro:AddEIDCollectible(
+        Astro.Collectible.CYGNUS,
+        "백조자리",
+        "...",
+        "게임 시간 15초마다 2번 빛줄기를 소환합니다." ..
+        "#중첩 시 발동 간격이 줄어듭니다." ..
+        "#패널티 피격 시 2분 동안 효과가 중지됩니다."
+    )
 end
 
 Astro:AddCallback(
@@ -10,7 +23,9 @@ Astro:AddCallback(
     ---@param player EntityPlayer
     function(_, player)
         if player:HasCollectible(Astro.Collectible.CYGNUS) then
-            if Game():GetFrameCount() % math.floor(450 / player:GetCollectibleNum(Astro.Collectible.CYGNUS)) == 0 then
+            local frameCount = Game():GetFrameCount()
+
+            if frameCount % math.floor(450 / player:GetCollectibleNum(Astro.Collectible.CYGNUS)) == 0 and Astro:GetLastPenaltyFrame(player) + PENALTY_TIME < frameCount then
                 player:UseActiveItem(160, false, true, false, false)
                 player:UseActiveItem(160, false, true, false, false)
             end
