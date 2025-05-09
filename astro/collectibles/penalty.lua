b1d589eec2e7f6668017bf744d93396cbbc0111e
Pdom <pdom0327@gmail.com>
2025-05-10 01:02:29 +0900

    feat: 패널티 추가

--- astro/collectibles/penalty.lua
+++ astro/collectibles/penalty.lua
@@ -1,3 +1,9 @@
+local PENALTY_DEATHS_HEAD_COUNT = 1 -- 빠가지 소환 개수
+
+local PENALTY_DEATHS_HEAD_CHANCE = 1 -- 빠가지 소환 확률
+
+local PENALTY_BONE_HEARTS = 1 -- 뼈 심장 추가 개수
+
 -- 패널티 피격 시 차지 감소
 local PENALTY_ACTIVE_ITEMS = {
     [CollectibleType.COLLECTIBLE_BOOK_OF_SHADOWS] = 2, -- ex) 피격 시 2 감소
@@ -40,6 +46,39 @@ if EID then
     end
 end
 
+local function CheckBossRoom()
+    local level = Game():GetLevel()
+    local stage = level:GetAbsoluteStage()
+    local currentRoom = level:GetCurrentRoom()
+    local currentRoomDesc = Game():GetLevel():GetCurrentRoomDesc()
+
+    if currentRoom:GetType() ~= RoomType.ROOM_BOSS then
+        return false
+    end
+
+    if currentRoom:IsClear() then
+        return false
+    end
+
+    if stage == LevelStage.STAGE3_2 and currentRoom:GetBossID() == 6 then
+        return true -- 엄마 발
+    elseif stage == LevelStage.STAGE4_3 then
+        return true -- 허쉬
+    elseif stage == LevelStage.STAGE5 then
+        return true -- 성당 / 저승
+    elseif stage == LevelStage.STAGE6 then
+        return true -- 체스트 / 다크룸 / 메가 사탄
+    elseif stage == LevelStage.STAGE7 then
+        return true -- 델리리움
+    elseif currentRoom:GetBossID() == 88 then
+        return true -- 마더
+    elseif currentRoomDesc.Data.Name == "Beast Room" then
+        return true -- 비스트
+    end
+
+    return false
+end
+
 Astro:AddCallback(
     Astro.Callbacks.POST_PLAYER_TAKE_PENALTY,
     ---@param player EntityPlayer
@@ -62,6 +101,18 @@ Astro:AddCallback(
         for _, item in ipairs(PENALTY_REMOVE_ITEMS) do
             if player:HasCollectible(item) then
                 Astro:RemoveAllCollectible(player, item)
+            end
+        end
+
+        if CheckBossRoom() then
+            if player:GetCollectibleRNG(Astro.Collectible.BIRTHRIGHT_EDEN_B):RandomFloat() <= PENALTY_DEATHS_HEAD_CHANCE then
+                for i = 1, PENALTY_DEATHS_HEAD_COUNT do
+                    Astro:Spawn(EntityType.ENTITY_DEATHS_HEAD, 0, 0, player.Position)
+                end
+            end
+
+            if not (Astro.IsFight and Astro:IsLatterStage()) then
+                player:AddBoneHearts(PENALTY_BONE_HEARTS)
             end
         end
     end
