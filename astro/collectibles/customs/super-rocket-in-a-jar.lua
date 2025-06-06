1e84425ecfa780b36640cfb7adeb1f692770dd78
Pdom <pdom0327@gmail.com>
2025-06-06 22:44:03 +0900

    feat(super-rocket-in-a-jar): 신규 아이템

---
+++ astro/collectibles/customs/super-rocket-in-a-jar.lua
@@ -0,0 +1,62 @@
+---
+
+local UPGRADE_CHANCE = 0.2
+
+---
+
+local hiddenItemManager = require("astro.lib.hidden_item_manager")
+
+Astro.Collectible.SUPER_ROCKET_IN_A_JAR = Isaac.GetItemIdByName("Super Rocket in a Jar")
+
+Astro:AddCallback(
+    Astro.Callbacks.MOD_INIT,
+    function(_)
+        if EID then
+            Astro:AddEIDCollectible(
+                Astro.Collectible.SUPER_ROCKET_IN_A_JAR,
+                "슈퍼 로켓 든 병",
+                "...",
+                "#{{Collectible583}} Rocket in a Jar 효과가 적용됩니다." ..
+                "#로켓이 기가 로켓으로 변경됩니다."
+            )
+        end
+    end
+)
+
+Astro:AddCallback(
+    ModCallbacks.MC_POST_GET_COLLECTIBLE,
+    ---@param selectedCollectible CollectibleType
+    ---@param itemPoolType ItemPoolType
+    ---@param decrease boolean
+    ---@param seed integer
+    function(_, selectedCollectible, itemPoolType, decrease, seed)
+        if selectedCollectible == CollectibleType.COLLECTIBLE_ROCKET_IN_A_JAR then
+            local rng = Isaac.GetPlayer():GetCollectibleRNG(Astro.Collectible.SUPER_ROCKET_IN_A_JAR)
+
+            if rng:RandomFloat() < UPGRADE_CHANCE then
+                return Astro.Collectible.SUPER_ROCKET_IN_A_JAR
+            end
+        end
+    end
+)
+
+Astro:AddCallback(
+    ModCallbacks.MC_POST_BOMB_INIT,
+    ---@param bomb EntityBomb
+    function(_, bomb)
+        local player = bomb.SpawnerEntity and bomb.SpawnerEntity:ToPlayer()
+        if player and player:HasCollectible(Astro.Collectible.SUPER_ROCKET_IN_A_JAR) then
+            Isaac.Spawn(EntityType.ENTITY_BOMB, BombVariant.BOMB_ROCKET_GIGA, 0, bomb.Position, bomb.Velocity, bomb.SpawnerEntity)
+            bomb:Remove()
+        end
+    end,
+    19
+)
+
+Astro:AddCallback(
+    ModCallbacks.MC_POST_PEFFECT_UPDATE,
+    ---@param player EntityPlayer
+    function(_, player)
+        hiddenItemManager:CheckStack(player, CollectibleType.COLLECTIBLE_ROCKET_IN_A_JAR, player:HasCollectible(Astro.Collectible.SUPER_ROCKET_IN_A_JAR) and 1 or 0, "ASTRO_ASTRO_SUPER_ROCKET_IN_A_JAR")
+    end
+)
