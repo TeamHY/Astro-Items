6664baed067474a0759bbdce212a229094907343
Pdom <pdom0327@gmail.com>
2025-06-15 23:59:06 +0900

    feat(ez-mode): 감소량 제한

--- astro/collectibles/customs/ez-mode.lua
+++ astro/collectibles/customs/ez-mode.lua
@@ -1,23 +1,29 @@
 ---
 
 local SPEED_DECREMENT = -0.025
+local LIMIT_SPEED_DECREMENT = -1.0
 
 local TEARS_DECREMENT = -0.005
+local LIMIT_TEARS_DECREMENT = -1.0
 
 local DAMAGE_DECREMENT = -0.05
+local LIMIT_DAMAGE_DECREMENT = -1.0
 
 local RANGE_DECREMENT = -0.05
+local LIMIT_RANGE_DECREMENT = -1.0
 
 local SHOT_SPEED_DECREMENT = -0.05
+local LIMIT_SHOT_SPEED_DECREMENT = -1.0
 
 local LUCK_DECREMENT = -3
+local LIMIT_LUCK_DECREMENT = -10
 
 ---
 
 Astro.Collectible.EZ_MODE = Isaac.GetItemIdByName("EZ Mode")
 
 if EID then
-    Astro:AddEIDCollectible(Astro.Collectible.EZ_MODE, "쉬운 모드", "...", "↓ {{LuckSmall}}행운 -1#후반 스테이지 진입 전까지 피격 페널티가 발생하지 않습니다. 대신 모든 스탯이 감소됩니다.#소울 하트 1개가 증가됩니다.")
+    Astro:AddEIDCollectible(Astro.Collectible.EZ_MODE, "쉬운 모드", "...", "↓ {{LuckSmall}}행운 -1#후반 스테이지 진입 전까지 피격 페널티가 발생하지 않습니다. 대신 모든 스탯이 감소됩니다. 각 스탯마다 감소량에 제한이 있습니다.#소울 하트 1개가 증가됩니다.")
 end
 
 Astro:AddCallback(
@@ -36,14 +42,13 @@ Astro:AddCallback(
                     shotSpeed = 0,
                     luck = 0
                 }
-            end
-
-            data["ezModeStatus"].speed = data["ezModeStatus"].speed + SPEED_DECREMENT
-            data["ezModeStatus"].tears = data["ezModeStatus"].tears + TEARS_DECREMENT
-            data["ezModeStatus"].damage = data["ezModeStatus"].damage + DAMAGE_DECREMENT
-            data["ezModeStatus"].range = data["ezModeStatus"].range + RANGE_DECREMENT
-            data["ezModeStatus"].shotSpeed = data["ezModeStatus"].shotSpeed + SHOT_SPEED_DECREMENT
-            data["ezModeStatus"].luck = data["ezModeStatus"].luck + LUCK_DECREMENT
+            end            -- Apply stat decrements with limits
+            data["ezModeStatus"].speed = math.max(data["ezModeStatus"].speed + SPEED_DECREMENT, LIMIT_SPEED_DECREMENT)
+            data["ezModeStatus"].tears = math.max(data["ezModeStatus"].tears + TEARS_DECREMENT, LIMIT_TEARS_DECREMENT)
+            data["ezModeStatus"].damage = math.max(data["ezModeStatus"].damage + DAMAGE_DECREMENT, LIMIT_DAMAGE_DECREMENT)
+            data["ezModeStatus"].range = math.max(data["ezModeStatus"].range + RANGE_DECREMENT, LIMIT_RANGE_DECREMENT)
+            data["ezModeStatus"].shotSpeed = math.max(data["ezModeStatus"].shotSpeed + SHOT_SPEED_DECREMENT, LIMIT_SHOT_SPEED_DECREMENT)
+            data["ezModeStatus"].luck = math.max(data["ezModeStatus"].luck + LUCK_DECREMENT, LIMIT_LUCK_DECREMENT)
 
             player:AddCacheFlags(CacheFlag.CACHE_ALL)
             player:EvaluateItems()
