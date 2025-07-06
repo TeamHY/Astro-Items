66f1eaf7381012b11ae55479980c48933fa0e791
Pdom <pdom0327@gmail.com>
2025-07-06 15:03:04 +0900

    feat(delirium-guppy): 하얀 파리 -> 파란 파리

--- astro/collectibles/customs/extra-guppy-sets.lua
+++ astro/collectibles/customs/extra-guppy-sets.lua
@@ -4,10 +4,9 @@ Astro.Collectible.ABSOLUT_GUPPY = Isaac.GetItemIdByName("Absolut Guppy")
 Astro.Collectible.DELIRIUM_GUPPY = Isaac.GetItemIdByName("Delirium Guppy")
 
 Astro:AddEIDCollectible(Astro.Collectible.ABSOLUT_GUPPY, "앱솔루트 구피", "...", "파란 아군 파리가 2배로 소환됩니다.")
-Astro:AddEIDCollectible(Astro.Collectible.DELIRIUM_GUPPY, "섬망 구피", "...", "획득 시 {{Trinket117}}Locust of Conquest를 소환합니다.#파란 아군 파리 대신 하얀 아군 파리가 소환됩니다.#적이 죽은 위치에 하얀 아군 파리가 소환됩니다.")
+Astro:AddEIDCollectible(Astro.Collectible.DELIRIUM_GUPPY, "섬망 구피", "...", "획득 시 {{Trinket117}}Locust of Conquest를 소환합니다.#적이 죽은 위치에 파란 아군 파리가 소환됩니다.")
 
 local ABSOLUT_GUPPY_SUBTYPE = 1000
-local DELIRIUM_FLY_COLOR = Color(1, 1, 1, 1, 1, 1, 1)
 
 local isStarted = false
 
@@ -31,10 +30,8 @@ Astro:AddCallback(
     ---@param familiar EntityFamiliar
     function(_, familiar)
         if familiar.Variant == FamiliarVariant.BLUE_FLY and familiar.SubType == 0 then
-            local absolutFly;
-
             if isStarted and Astro:HasCollectible(Astro.Collectible.ABSOLUT_GUPPY) then
-                absolutFly = Isaac.Spawn(
+                Isaac.Spawn(
                     EntityType.ENTITY_FAMILIAR,
                     FamiliarVariant.BLUE_FLY,
                     ABSOLUT_GUPPY_SUBTYPE,
@@ -43,16 +40,6 @@ Astro:AddCallback(
                     familiar.SpawnerEntity
                 )
             end
-
-            if Astro:HasCollectible(Astro.Collectible.DELIRIUM_GUPPY) then
-                familiar.Color = DELIRIUM_FLY_COLOR
-
-                if absolutFly then
-                    absolutFly.Color = DELIRIUM_FLY_COLOR
-                end
-            end
-        elseif familiar.Variant == FamiliarVariant.BLUE_FLY and familiar.SubType == ABSOLUT_GUPPY_SUBTYPE then
-            familiar.Color = DELIRIUM_FLY_COLOR
         end
     end
 )
@@ -70,8 +57,6 @@ Astro:AddCallback(
                 Vector.Zero,
                 npc
             )
-
-            blueFly.Color = DELIRIUM_FLY_COLOR
         end
     end
 )
