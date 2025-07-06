f316af1997eb7bba7c36b3879c7c0d9a6e8211bc
Pdom <pdom0327@gmail.com>
2025-07-06 17:54:45 +0900

    feat(obsidian-hand): 신규 장신구

---
+++ astro/trinkets/customs/obsidian-hand.lua
@@ -0,0 +1,27 @@
+---
+
+Astro.Trinket.OBSIDIAN_HAND = Isaac.GetTrinketIdByName("Obsidian Hand")
+
+if EID then
+    EID:addTrinket(Astro.Trinket.OBSIDIAN_HAND, "스테이지 입장 시 {{Collectible479}}Smelter 1회를 발동합니다.", "옵시디언 손")
+
+-- Astro:AddGoldenTrinketDescription(Astro.Trinket.OBSIDIAN_HAND, "", 10)
+end
+
+---@param player EntityPlayer
+function Astro:ActivateObsidianHandEffect(player)
+    player:UseActiveItem(CollectibleType.COLLECTIBLE_SMELTER, UseFlag.USE_NOANIM)
+end
+
+Astro:AddCallback(
+    ModCallbacks.MC_POST_NEW_LEVEL,
+    function()
+        for i = 0, Game():GetNumPlayers() - 1 do
+            local player = Game():GetPlayer(i)
+            
+            if player:HasTrinket(Astro.Trinket.OBSIDIAN_HAND) then
+                Astro:ActivateObsidianHandEffect(player)
+            end
+        end
+    end
+)
