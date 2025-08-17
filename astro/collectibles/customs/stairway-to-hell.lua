a9777a1f14ab6da76d9300f85380cf17da4bee89
sawalk <76607639+sawalk@users.noreply.github.com>
2025-08-18 06:13:08 +0900

    Update stairway-to-hell.lua

--- astro/collectibles/customs/stairway-to-hell.lua
+++ astro/collectibles/customs/stairway-to-hell.lua
@@ -9,7 +9,7 @@ Astro:AddCallback(
             Astro:AddEIDCollectible(
                 Astro.Collectible.STAIRWAY_TO_HELL,
                 "지옥의 계단",
-                "...",
+                "기다린 것을 얻을 수 있기를",
                 "스테이지 입장 시 {{DevilRoom}}악마방으로 이동 가능한 계단이 생성됩니다. 방을 이동하면 사라집니다."
             )
         end
