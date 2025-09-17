943df1345fe5658404c4cc13818a38a543c984e8
Pdom <pdom0327@gmail.com>
2025-09-17 17:04:08 +0900

    fix(black-mirror): 롤백

--- astro/trinkets/customs/black-mirror.lua
+++ astro/trinkets/customs/black-mirror.lua
@@ -7,6 +7,7 @@ if EID then
         Astro.Trinket.BLACK_MIRROR,
         "검은 거울",
         "모든 벽, 모든 책상, 모든 손바닥에 있는",
+        "#!!! 획득한 즉시 흡수됨" ..
         "{{Collectible347}} 패시브 아이템 획득 시 그 아이템을 한번 더 획득하며;" ..
         "#{{ArrowGrayRight}} 획득 이후 이 장신구는 사라집니다."
     )
@@ -14,22 +15,22 @@ if EID then
     -- Astro:AddGoldenTrinketDescription(Astro.Trinket.BLACK_MIRROR, "", 10)
 end
 
--- Astro:AddCallback(
---     ModCallbacks.MC_POST_PEFFECT_UPDATE,
---     ---@param player EntityPlayer
---     function(_, player)
---         local trinket0 = player:GetTrinket(0)
---         local trinket1 = player:GetTrinket(1)
+Astro:AddCallback(
+    ModCallbacks.MC_POST_PEFFECT_UPDATE,
+    ---@param player EntityPlayer
+    function(_, player)
+        local trinket0 = player:GetTrinket(0)
+        local trinket1 = player:GetTrinket(1)
 
---         if Astro:CheckTrinket(trinket0, Astro.Trinket.BLACK_MIRROR) then
---             player:TryRemoveTrinket(trinket0)
---             isc:smeltTrinket(player, Astro.Trinket.BLACK_MIRROR)
---         elseif Astro:CheckTrinket(trinket1, Astro.Trinket.BLACK_MIRROR) then
---             player:TryRemoveTrinket(trinket1)
---             isc:smeltTrinket(player, Astro.Trinket.BLACK_MIRROR)
---         end
---     end
--- )
+        if Astro:CheckTrinket(trinket0, Astro.Trinket.BLACK_MIRROR) then
+            player:TryRemoveTrinket(trinket0)
+            isc:smeltTrinket(player, Astro.Trinket.BLACK_MIRROR)
+        elseif Astro:CheckTrinket(trinket1, Astro.Trinket.BLACK_MIRROR) then
+            player:TryRemoveTrinket(trinket1)
+            isc:smeltTrinket(player, Astro.Trinket.BLACK_MIRROR)
+        end
+    end
+)
 
 Astro:AddCallbackCustom(
     isc.ModCallbackCustom.POST_PLAYER_COLLECTIBLE_ADDED,
