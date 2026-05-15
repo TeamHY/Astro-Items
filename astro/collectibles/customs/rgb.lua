ea9621d72d45f5086b7f77b63c2ecfc2caeb79fe
Pdom <pdom0327@gmail.com>
2026-05-15 12:04:35 +0900

    chore(rgb): 방 대신 캐릭터 색상 변경

--- astro/collectibles/customs/rgb.lua
+++ astro/collectibles/customs/rgb.lua
@@ -1,6 +1,6 @@
 ---
 
-local COLOR_VALUE = 0.1 -- 방 색상 강도 (0.0 ~ 1.0)
+local COLOR_VALUE = 0.2 -- 캐릭터 색상 강도 (0.0 ~ 1.0)
 
 local DAMAGE_MULTIPLIER = 1.4
 
@@ -22,7 +22,7 @@ Astro:AddCallback(
                 Astro.Collectible.RGB,
                 "삼원색",
                 "불 좀 꺼줄래?",
-                "방의 색상을 변경하며 색상에 따라 효과가 달라집니다:" ..
+                "방마다 캐릭터의 색상이 변경되며 색상에 따라 효과가 달라집니다:" ..
                 "#{{ArrowGrayRight}} {{ColorRed}}(빨강){{CR}} {{DamageSmall}}공격력 배율 x1.4" ..
                 "#{{ArrowGrayRight}} {{ColorGreen}}(초록){{CR}} {{LuckSmall}}행운 배율 x1.5" ..
                 "#{{ArrowGrayRight}} {{ColorBlue}}(파랑){{CR}} {{TearsSmall}}연사 배율 x1.2",
@@ -48,26 +48,13 @@ local function GetColor()
 end
 
 local function UpdateColor()
-    local room = Game():GetLevel():GetCurrentRoom()
-    local color = GetColor()
-
-    if color == colors.RED then
-        room:SetWallColor(Color(1, 1, 1, 1, COLOR_VALUE, 0, 0))
-        room:SetFloorColor(Color(1, 1, 1, 1, COLOR_VALUE, 0, 0))
-    elseif color == colors.GREEN then
-        room:SetWallColor(Color(1, 1, 1, 1, 0, COLOR_VALUE, 0))
-        room:SetFloorColor(Color(1, 1, 1, 1, 0, COLOR_VALUE, 0))
-    else
-        room:SetWallColor(Color(1, 1, 1, 1, 0, 0, COLOR_VALUE))
-        room:SetFloorColor(Color(1, 1, 1, 1, 0, 0, COLOR_VALUE))
-    end
-
     for i = 1, Game():GetNumPlayers() do
         local player = Isaac.GetPlayer(i - 1)
     
         player:AddCacheFlags(CacheFlag.CACHE_DAMAGE)
         player:AddCacheFlags(CacheFlag.CACHE_LUCK)
         player:AddCacheFlags(CacheFlag.CACHE_FIREDELAY)
+        player:AddCacheFlags(CacheFlag.CACHE_COLOR)
         player:EvaluateItems()
     end
 end
@@ -106,14 +93,20 @@ Astro:AddPriorityCallback(
         if color == colors.RED then
             if cacheFlag == CacheFlag.CACHE_DAMAGE then
                 player.Damage = player.Damage * (DAMAGE_MULTIPLIER ^ rgbNum)
+            elseif cacheFlag == CacheFlag.CACHE_COLOR then
+                player.Color = Color(1, 1, 1, 1, COLOR_VALUE, 0, 0)
             end
         elseif color == colors.GREEN then
             if cacheFlag == CacheFlag.CACHE_LUCK and player.Luck > 0 then
                 player.Luck = player.Luck * (LUCK_MULTIPLIER ^ rgbNum)
+            elseif cacheFlag == CacheFlag.CACHE_COLOR then
+                player.Color = Color(1, 1, 1, 1, 0, COLOR_VALUE, 0)
             end
         else
             if cacheFlag == CacheFlag.CACHE_FIREDELAY then
                 player.MaxFireDelay = ((player.MaxFireDelay + 1) / (FIRE_DELAY_MULTIPLIER ^ rgbNum)) - 1
+            elseif cacheFlag == CacheFlag.CACHE_COLOR then
+                player.Color = Color(1, 1, 1, 1, 0, 0, COLOR_VALUE)
             end
         end
     end
