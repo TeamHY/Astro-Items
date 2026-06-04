a4f3742b8b488d1dd0161be52e8696496ec414b0
sawalk <ham.step2@gmail.com>
2026-06-04 23:33:25 +0900

    feat(mineral-shard): 누락 수정

--- astro/collectibles/customs/mineral-shard.lua
+++ astro/collectibles/customs/mineral-shard.lua
@@ -14,8 +14,7 @@ Astro:AddCallback(
                 Astro.Collectible.MINERAL_SHARD,
                 "미네랄 조각",
                 "별의 선물",
-                "{{Collectible132}} 공격이 멀리 나갈수록 타일 1칸당 피해량이 공격력 x0.6 증가합니다." ..
-                "#눈물의 피해량이 초당 +15.5 증가합니다.",
+                "{{Collectible132}} 눈물이 멀리 나갈수록 프레임당 공격력 x0.004씩 증가합니다.",
                 -- 중첩 시
                 "중첩 시 눈물 피해량의 상승량이 증가"
             )
@@ -23,8 +22,7 @@ Astro:AddCallback(
             Astro.EID:AddCollectible(
                 Astro.Collectible.MINERAL_SHARD,
                 "Mineral Shard", "",
-                "{{Collectible132}} Attacks deal +0.6 damage per tile the further they travel" ..
-                "{{Damage}} Tears deal +15.5 damage per seconds",
+                "{{Collectible132}} Tears deal 0.004x Isaac's damage per frames the further they travel",
                 -- Stacks
                 "Stackable",
                 "en_us"
@@ -69,13 +67,15 @@ Astro:AddCallback(
         local player = Astro:GetPlayerFromEntity(tear)
 
         if player and player:HasCollectible(Astro.Collectible.MINERAL_SHARD) then
+            local num = player:GetCollectibleNum(Astro.Collectible.MINERAL_SHARD)
+
             if tear:HasTearFlags(TearFlags.TEAR_LUDOVICO) then
                 local tearParams = player:GetTearHitParams(WeaponType.WEAPON_LUDOVICO_TECHNIQUE)
                 local tearDmg = tearParams.TearDamage
                 local tearPos = tear.Position
                 local parentPos = tear.Parent.Position
                 local dist = parentPos:Distance(tearPos)
-                local clampMax = math.max(100, 700 - tearDmg)
+                local clampMax = math.max(100, 700 - tearDmg - (num - 1) * 10)
                 local clamped = Astro:Clamp(dist, 1, clampMax)
                 local scaleFactor = clamped / clampMax + 1
 
@@ -88,7 +88,7 @@ Astro:AddCallback(
             elseif tear.WaitFrames < 1 then
                 local len = tear.PosDisplacement:Length()
                 
-                tear.CollisionDamage = tear.CollisionDamage + len * player.Damage * EXTRA_DAMAGE_MULTI
+                tear.CollisionDamage = tear.CollisionDamage + len * player.Damage * EXTRA_DAMAGE_MULTI * num
                 tear.Scale = tear.BaseScale + 0.01
             end
         end
